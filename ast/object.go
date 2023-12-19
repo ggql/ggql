8f58000647225a9cbff0bc700fabc01e53a57e2c
Jia <49358172+craftslab@users.noreply.github.com>
2023-12-19 12:11:18 +0000

    feat(ast): add support for object

--- ast/object.go
+++ ast/object.go
@@ -1,1 +1,29 @@
 package ast
+
+import (
+	"sync"
+)
+
+type GQLObject struct {
+	Attributes map[string]Value
+}
+
+type Value interface{}
+
+var mutex = &sync.Mutex{}
+
+func FlatGQLGroups(groups *[][]GQLObject) error {
+	var mainGroup []GQLObject
+
+	for _, group := range *groups {
+		mainGroup = append(mainGroup, group...)
+	}
+
+	mutex.Lock()
+	*groups = (*groups)[:0] // clear the groups slice
+	mutex.Unlock()
+
+	*groups = append(*groups, mainGroup)
+
+	return nil
+}
--- ast/object_test.go
+++ ast/object_test.go
@@ -1,1 +1,38 @@
 package ast
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFlatGQLGroups(t *testing.T) {
+	var group []GQLObject
+	var groups [][]GQLObject
+
+	err := FlatGQLGroups(&groups)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 1, len(groups))
+
+	for _, item := range groups {
+		assert.Equal(t, 0, len(item))
+	}
+
+	group = append(group, GQLObject{
+		Attributes: map[string]Value{
+			"key": "val",
+		},
+	})
+
+	groups = groups[:0]
+	groups = append(groups, group, group)
+	assert.Equal(t, 2, len(groups))
+
+	err = FlatGQLGroups(&groups)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 1, len(groups))
+
+	for _, item := range groups {
+		assert.Equal(t, 2, len(item))
+	}
+}
