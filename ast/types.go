68e0d678014402b6ebacfcb87e1df0e15d9d2878
Jia <49358172+craftslab@users.noreply.github.com>
2024-01-04 05:49:06 +0000

    feat(ast): add support for types

--- ast/types.go
+++ ast/types.go
@@ -1,1 +1,122 @@
 package ast
+
+// DataType represents the data types.
+type DataType int
+
+const (
+	Any DataType = iota
+	Text
+	Integer
+	Float
+	Boolean
+	Date
+	Time
+	DateTime
+	Undefined
+	Null
+)
+
+const (
+	AnyStr       = "Any"
+	TextStr      = "Text"
+	IntegerStr   = "Integer"
+	FloatStr     = "Float"
+	BooleanStr   = "Boolean"
+	DateStr      = "Date"
+	TimeStr      = "Time"
+	DateTimeStr  = "DateTime"
+	UndefinedStr = "Undefined"
+	NullStr      = "Null"
+	UnknownStr   = "Unknown"
+)
+
+// isType checks if the data type matches the given type.
+func (dt DataType) isType(dataType DataType) bool {
+	return dt == dataType
+}
+
+// isInt checks if the data type is Integer.
+func (dt DataType) isInt() bool {
+	return dt.isType(Integer)
+}
+
+// isFloat checks if the data type is Float.
+func (dt DataType) isFloat() bool {
+	return dt.isType(Float)
+}
+
+// isNumber checks if the data type is Integer or Float.
+func (dt DataType) isNumber() bool {
+	return dt.isInt() || dt.isFloat()
+}
+
+// isText checks if the data type is Text.
+func (dt DataType) isText() bool {
+	return dt.isType(Text)
+}
+
+// isTime checks if the data type is Time.
+func (dt DataType) isTime() bool {
+	return dt.isType(Time)
+}
+
+// isDate checks if the data type is Date.
+func (dt DataType) isDate() bool {
+	return dt.isType(Date)
+}
+
+// isDateTime checks if the data type is DateTime.
+func (dt DataType) isDateTime() bool {
+	return dt.isType(DateTime)
+}
+
+// isUndefined checks if the data type is Undefined.
+func (dt DataType) isUndefined() bool {
+	return dt.isType(Undefined)
+}
+
+// literal returns the string representation of the data type.
+func (dt DataType) literal() string {
+	switch dt {
+	case Any:
+		return AnyStr
+	case Text:
+		return TextStr
+	case Integer:
+		return IntegerStr
+	case Float:
+		return FloatStr
+	case Boolean:
+		return BooleanStr
+	case Date:
+		return DateStr
+	case Time:
+		return TimeStr
+	case DateTime:
+		return DateTimeStr
+	case Undefined:
+		return UndefinedStr
+	case Null:
+		return NullStr
+	default:
+		return UnknownStr
+	}
+}
+
+var tablesFieldsTypes = map[string]DataType{
+	"commit_id":     Text,
+	"title":         Text,
+	"message":       Text,
+	"name":          Text,
+	"full_name":     Text,
+	"insertions":    Integer,
+	"deletions":     Integer,
+	"files_changed": Integer,
+	"email":         Text,
+	"type":          Text,
+	"datetime":      DateTime,
+	"is_head":       Boolean,
+	"is_remote":     Boolean,
+	"commit_count":  Integer,
+	"repo":          Text,
+}
--- ast/types_test.go
+++ ast/types_test.go
@@ -1,1 +1,92 @@
 package ast
+
+import (
+	"testing"
+)
+
+func TestDataType(t *testing.T) {
+	// Test isType method
+	if !Any.isType(Any) {
+		t.Error("Any.isType(Any) should be true")
+	}
+	if Integer.isType(Float) {
+		t.Error("Integer.isType(Float) should be false")
+	}
+
+	// Test isInt method
+	if !Integer.isInt() {
+		t.Error("Integer.isInt should be true")
+	}
+
+	// Test isFloat method
+	if !Float.isFloat() {
+		t.Error("Float.isFloat should be true")
+	}
+
+	// Test isNumber method
+	if !Integer.isNumber() {
+		t.Error("Integer.isNumber should be true")
+	}
+	if !Float.isNumber() {
+		t.Error("Float.isNumber should be true")
+	}
+
+	// Test isText method
+	if !Text.isText() {
+		t.Error("Text.isText should be true")
+	}
+
+	// Test isTime method
+	if !Time.isTime() {
+		t.Error("Time.isTime should be true")
+	}
+
+	// Test isDate method
+	if !Date.isDate() {
+		t.Error("Date.isDate should be true")
+	}
+
+	// Test idDateTime method
+	if !DateTime.isDateTime() {
+		t.Error("DateTime.isDateTime should be true")
+	}
+
+	// Test idDateTime method
+	if !Undefined.isUndefined() {
+		t.Error("Undefined.isUndefined should be true")
+	}
+}
+
+func TestLiteral(t *testing.T) {
+	// Test literal method
+	if Any.literal() != "Any" {
+		t.Error("Any.literal() should be 'Any'")
+	}
+	if Text.literal() != "Text" {
+		t.Error("Text.literal() should be 'Text'")
+	}
+	if Integer.literal() != "Integer" {
+		t.Error("Integer.literal() should be 'Integer'")
+	}
+	if Float.literal() != "Float" {
+		t.Error("Float.literal() should be 'Float'")
+	}
+	if Boolean.literal() != "Boolean" {
+		t.Error("Boolean.literal() should be 'Boolean'")
+	}
+	if Date.literal() != "Date" {
+		t.Error("Date.literal() should be 'Date'")
+	}
+	if Time.literal() != "Time" {
+		t.Error("Time.literal() should be 'Time'")
+	}
+	if DateTime.literal() != "DateTime" {
+		t.Error("DateTime.literal() should be 'DateTime'")
+	}
+	if Undefined.literal() != "Undefined" {
+		t.Error("Undefined.literal() should be 'Undefined'")
+	}
+	if Null.literal() != "Null" {
+		t.Error("Null.literal() should be 'Null'")
+	}
+}
