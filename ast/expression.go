e86ad8b0b26dff74ca180a015cf3014d23bf37cc
Jia Jia <angersax@sina.com>
2024-01-11 23:54:25 +0800

    feat(ast): add support for expression

--- ast/expression.go
+++ ast/expression.go
@@ -1,1 +1,433 @@
 package ast
+
+type ExpressionKind int
+
+const (
+	ExprAssignment ExpressionKind = iota
+	ExprString
+	ExprSymbol
+	ExprGlobalVariable
+	ExprNumber
+	ExprBoolean
+	ExprPrefixUnary
+	ExprArithmetic
+	ExprComparison
+	ExprLike
+	ExprGlob
+	ExprLogical
+	ExprBitwise
+	ExprCall
+	ExprBetween
+	ExprCase
+	ExprIn
+	ExprIsNull
+	ExprNull
+)
+
+type Expression interface {
+	ExpressionKind() ExpressionKind
+	ExprType(scope *Environment) DataType
+	AsAny() interface{}
+}
+
+func IsConst(expr Expression) bool {
+	switch expr.ExpressionKind() {
+	case ExprNumber, ExprBoolean, ExprString:
+		return true
+	default:
+		return false
+	}
+}
+
+type StringValueType int
+
+const (
+	StringValueText StringValueType = iota
+	StringValueTime
+	StringValueDate
+	StringValueDateTime
+)
+
+type StringExpression struct {
+	Value     string
+	ValueType StringValueType
+}
+
+func (s *StringExpression) ExpressionKind() ExpressionKind {
+	return ExprString
+}
+
+func (s *StringExpression) ExprType(scope *Environment) DataType {
+	switch s.ValueType {
+	case StringValueText:
+		return Text
+	case StringValueTime:
+		return Time
+	case StringValueDate:
+		return Date
+	case StringValueDateTime:
+		return DateTime
+	default:
+		return Undefined
+	}
+}
+
+func (s *StringExpression) AsAny() interface{} {
+	return s
+}
+
+type SymbolExpression struct {
+	Value string
+}
+
+func (s *SymbolExpression) ExpressionKind() ExpressionKind {
+	return ExprSymbol
+}
+
+func (s *SymbolExpression) ExprType(scope *Environment) DataType {
+	if scope.Contains(s.Value) {
+		return scope.Scopes[s.Value].Clone()
+	}
+
+	if typ, ok := tablesFieldsTypes[s.Value]; ok {
+		return typ.Clone()
+	}
+
+	return Undefined
+}
+
+func (s *SymbolExpression) AsAny() interface{} {
+	return s
+}
+
+type GlobalVariableExpression struct {
+	Name string
+}
+
+func (s *GlobalVariableExpression) ExpressionKind() ExpressionKind {
+	return ExprGlobalVariable
+}
+
+func (s *GlobalVariableExpression) ExprType(scope *Environment) DataType {
+	if scope.Contains(s.Name) {
+		return scope.Scopes[s.Name].Clone()
+	}
+
+	if typ, ok := tablesFieldsTypes[s.Name]; ok {
+		return typ.Clone()
+	}
+
+	return Undefined
+}
+
+func (s *GlobalVariableExpression) AsAny() interface{} {
+	return s
+}
+
+type NumberExpression struct {
+	Value Value
+}
+
+func (s *NumberExpression) ExpressionKind() ExpressionKind {
+	return ExprNumber
+}
+
+func (s *NumberExpression) ExprType(scope *Environment) DataType {
+	return s.Value.DataType()
+}
+
+func (s *NumberExpression) AsAny() interface{} {
+	return s
+}
+
+type BooleanExpression struct {
+	IsTrue bool
+}
+
+func (s *BooleanExpression) ExpressionKind() ExpressionKind {
+	return ExprBoolean
+}
+
+func (s *BooleanExpression) ExprType(scope *Environment) DataType {
+	return Boolean
+}
+
+func (s *BooleanExpression) AsAny() interface{} {
+	return s
+}
+
+type PrefixUnaryOperator int
+
+const (
+	Minus PrefixUnaryOperator = iota
+	Bang
+)
+
+type PrefixUnary struct {
+	Right Expression
+	Op    PrefixUnaryOperator
+}
+
+func (s *PrefixUnary) ExpressionKind() ExpressionKind {
+	return ExprPrefixUnary
+}
+
+func (s *PrefixUnary) ExprType(scope *Environment) DataType {
+	if s.Op == Bang {
+		return Boolean
+	} else {
+		return Integer
+	}
+}
+
+func (s *PrefixUnary) AsAny() interface{} {
+	return s
+}
+
+type ArithmeticOperator int
+
+const (
+	AOPlus ArithmeticOperator = iota
+	AOMinus
+	AOStar
+	AOSlash
+	AOModulus
+)
+
+type ArithmeticExpression struct {
+	Left     Expression
+	Operator ArithmeticOperator
+	Right    Expression
+}
+
+func (s *ArithmeticExpression) ExpressionKind() ExpressionKind {
+	return ExprArithmetic
+}
+
+func (s *ArithmeticExpression) ExprType(scope *Environment) DataType {
+	lhs := s.Left.ExprType(scope)
+	rhs := s.Right.ExprType(scope)
+
+	if lhs.isInt() && rhs.isInt() {
+		return Integer
+	}
+
+	return Float
+}
+
+func (s *ArithmeticExpression) AsAny() interface{} {
+	return s
+}
+
+type ComparisonOperator int
+
+const (
+	COGreater ComparisonOperator = iota
+	COGreaterEqual
+	COLess
+	COLessEqual
+	COEqual
+	CONotEqual
+	CONullSafeEqual
+)
+
+type ComparisonExpression struct {
+	Left     Expression
+	Operator ComparisonOperator
+	Right    Expression
+}
+
+func (s *ComparisonExpression) ExpressionKind() ExpressionKind {
+	return ExprComparison
+}
+
+func (s *ComparisonExpression) ExprType(scope *Environment) DataType {
+	if s.Operator == CONullSafeEqual {
+		return Integer
+	} else {
+		return Boolean
+	}
+}
+
+func (s *ComparisonExpression) AsAny() interface{} {
+	return s
+}
+
+type LikeExpression struct {
+	Input   Expression
+	Pattern Expression
+}
+
+func (s *LikeExpression) ExpressionKind() ExpressionKind {
+	return ExprLike
+}
+
+func (s *LikeExpression) ExprType(scope *Environment) DataType {
+	return Boolean
+}
+
+func (s *LikeExpression) AsAny() interface{} {
+	return s
+}
+
+type GlobExpression struct {
+	Input   Expression
+	Pattern Expression
+}
+
+func (s *GlobExpression) ExpressionKind() ExpressionKind {
+	return ExprGlob
+}
+
+func (s *GlobExpression) ExprType(scope *Environment) DataType {
+	return Boolean
+}
+
+func (s *GlobExpression) AsAny() interface{} {
+	return s
+}
+
+type LogicalOperator int
+
+const (
+	Or LogicalOperator = iota
+	And
+	Xor
+)
+
+type LogicalExpression struct {
+	Left     Expression
+	Operator LogicalOperator
+	Right    Expression
+}
+
+func (s *LogicalExpression) ExpressionKind() ExpressionKind {
+	return ExprLogical
+}
+
+func (s *LogicalExpression) ExprType(scope *Environment) DataType {
+	return Boolean
+}
+
+func (s *LogicalExpression) AsAny() interface{} {
+	return s
+}
+
+type BitwiseOperator int
+
+const (
+	BOOr BitwiseOperator = iota
+	BOAnd
+	BORightShift
+	BOLeftShift
+)
+
+type BitwiseExpression struct {
+	Left     Expression
+	Operator BitwiseOperator
+	Right    Expression
+}
+
+func (s *BitwiseExpression) ExpressionKind() ExpressionKind {
+	return ExprBitwise
+}
+
+func (s *BitwiseExpression) ExprType(scope *Environment) DataType {
+	return Integer
+}
+
+func (s *BitwiseExpression) AsAny() interface{} {
+	return s
+}
+
+type CallExpression struct {
+	FunctionName  string
+	Arguments     []Expression
+	IsAggregation bool
+}
+
+type BetweenExpression struct {
+	Value      Expression
+	RangeStart Expression
+	RangeEnd   Expression
+}
+
+func (s *BetweenExpression) ExpressionKind() ExpressionKind {
+	return ExprBetween
+}
+
+func (s *BetweenExpression) ExprType(scope *Environment) DataType {
+	return Boolean
+}
+
+func (s *BetweenExpression) AsAny() interface{} {
+	return s
+}
+
+type CaseExpression struct {
+	Conditions   []Expression
+	Values       []Expression
+	DefaultValue Expression
+	ValuesType   DataType
+}
+
+func (s *CaseExpression) ExpressionKind() ExpressionKind {
+	return ExprCase
+}
+
+func (s *CaseExpression) ExprType(scope *Environment) DataType {
+	return Boolean
+}
+
+func (s *CaseExpression) AsAny() interface{} {
+	return s
+}
+
+type InExpression struct {
+	Argument   Expression
+	Values     []Expression
+	ValuesType DataType
+}
+
+func (s *InExpression) ExpressionKind() ExpressionKind {
+	return ExprIn
+}
+
+func (s *InExpression) ExprType(scope *Environment) DataType {
+	return s.ValuesType.Clone()
+}
+
+func (s *InExpression) AsAny() interface{} {
+	return s
+}
+
+type IsNullExpression struct {
+	Argument Expression
+	HasNot   bool
+}
+
+func (s *IsNullExpression) ExpressionKind() ExpressionKind {
+	return ExprIsNull
+}
+
+func (s *IsNullExpression) ExprType(scope *Environment) DataType {
+	return Boolean
+}
+
+func (s *IsNullExpression) AsAny() interface{} {
+	return s
+}
+
+type NullExpression struct{}
+
+func (s *NullExpression) ExpressionKind() ExpressionKind {
+	return ExprNull
+}
+
+func (s *NullExpression) ExprType(scope *Environment) DataType {
+	return Null
+}
+
+func (s *NullExpression) AsAny() interface{} {
+	return s
+}
--- ast/expression_test.go
+++ ast/expression_test.go
@@ -1,1 +1,433 @@
 package ast
+
+import "testing"
+
+func TestStringExpressionExpressionKind(t *testing.T) {
+	testEnvironment := &Environment{
+		Scopes: make(map[string]DataType),
+	}
+
+	strExpr := &StringExpression{
+		Value:     "value",
+		ValueType: StringValueText,
+	}
+
+	if kind := strExpr.ExpressionKind(); kind != ExprString {
+		t.Errorf("Expected stringExpression %v, got %v", ExprString, kind)
+	}
+
+	dataType := strExpr.ExprType(testEnvironment)
+	if dataType != Text {
+		t.Errorf("ExprType() returned %v, expected %v", dataType, Text)
+	}
+
+	anyValue := strExpr.AsAny()
+	if anyValue != strExpr {
+		t.Errorf("AsAny() returned %v, expected %v", anyValue, strExpr)
+	}
+}
+
+func TestSymbolExpressionExpressionKind(t *testing.T) {
+	testEnvironment := &Environment{
+		Scopes: make(map[string]DataType),
+	}
+
+	testEnvironment.Scopes["someSymbol"] = Text
+
+	sblExpr := &SymbolExpression{
+		Value: "someSymbol",
+	}
+
+	if kind := sblExpr.ExpressionKind(); kind != ExprSymbol {
+		t.Errorf("Expected SymbolExpression %v, got %v", ExprSymbol, kind)
+	}
+
+	if dataType := sblExpr.ExprType(testEnvironment); dataType != Text {
+		t.Errorf("ExprType() returned %v, expected %v", dataType, Text)
+	}
+
+	if anyValue := sblExpr.AsAny(); anyValue != sblExpr {
+		t.Errorf("AsAny() returned %v, expected %v", anyValue, sblExpr)
+	}
+}
+
+func TestGlobalVariableExpressionKind(t *testing.T) {
+	testEnvironment := &Environment{
+		Scopes: make(map[string]DataType),
+	}
+
+	gvExpr := &GlobalVariableExpression{
+		Name: "globalname",
+	}
+
+	if kind3 := gvExpr.ExpressionKind(); kind3 != ExprGlobalVariable {
+		t.Errorf("Expected GlobalVariableExpression %v, got %v", ExprGlobalVariable, kind3)
+	}
+
+	testEnvironment.Scopes["globalname"] = Text
+	dataType3 := gvExpr.ExprType(testEnvironment)
+	if dataType3 != Text {
+		t.Errorf("ExprType() returned %v, expected %v", dataType3, Text)
+	}
+
+	if anyValue3 := gvExpr.AsAny(); anyValue3 != gvExpr {
+		t.Errorf("AsAny() returned %v, expected %v", anyValue3, gvExpr)
+	}
+}
+
+func TestNumberExpressionKind(t *testing.T) {
+	testEnvironment := &Environment{
+		Scopes: make(map[string]DataType),
+	}
+
+	testEnvironment.Scopes["someNumber"] = Text
+
+	numberExpr := &NumberExpression{
+		Value: TextValue{},
+	}
+
+	if kind := numberExpr.ExpressionKind(); kind != ExprNumber {
+		t.Errorf("Expected NumberExpression %v, got %v", ExprNumber, kind)
+	}
+
+	if dataType := numberExpr.ExprType(testEnvironment); dataType != Text {
+		t.Errorf("ExprType() returned %v, expected %v", dataType, Text)
+	}
+
+	if anyValue := numberExpr.AsAny(); anyValue != numberExpr {
+		t.Errorf("AsAny() returned %v, expected %v", anyValue, numberExpr)
+	}
+}
+
+func TestBooleanExpressionKind(t *testing.T) {
+	testEnvironment := &Environment{
+		Scopes: make(map[string]DataType),
+	}
+
+	booleanExpr := &BooleanExpression{
+		IsTrue: true,
+	}
+
+	if kind := booleanExpr.ExpressionKind(); kind != ExprBoolean {
+		t.Errorf("Expected BooleanExpression %v, got %v", ExprBoolean, kind)
+	}
+
+	if dataType := booleanExpr.ExprType(testEnvironment); dataType != Boolean {
+		t.Errorf("ExprType() returned %v, expected %v", dataType, Boolean)
+	}
+
+	if anyValue := booleanExpr.AsAny(); anyValue != booleanExpr {
+		t.Errorf("AsAny() returned %v, expected %v", anyValue, booleanExpr)
+	}
+}
+
+func TestPrefixUnaryExpressionKind(t *testing.T) {
+	testEnvironment := &Environment{
+		Scopes: make(map[string]DataType),
+	}
+
+	prefixUnary := &PrefixUnary{
+		Right: &PrefixUnary{},
+		Op:    Bang,
+	}
+
+	if kind := prefixUnary.ExpressionKind(); kind != ExprPrefixUnary {
+		t.Errorf("Expected PrefixUnary %v, got %v", ExprPrefixUnary, kind)
+	}
+
+	if dataType := prefixUnary.ExprType(testEnvironment); dataType != Boolean {
+		t.Errorf("ExprType() returned %v, expected %v", dataType, Boolean)
+	}
+
+	if anyValue := prefixUnary.AsAny(); anyValue != prefixUnary {
+		t.Errorf("AsAny() returned %v, expected %v", anyValue, prefixUnary)
+	}
+}
+
+func TestArithmeticExpressionKind(t *testing.T) {
+	testEnvironment := &Environment{
+		Scopes: make(map[string]DataType),
+	}
+
+	arithmeticExpr := &ArithmeticExpression{
+		Left: &InExpression{
+			ValuesType: Integer,
+		},
+		Operator: AOPlus,
+		Right: &InExpression{
+			ValuesType: Integer,
+		},
+	}
+
+	if kind := arithmeticExpr.ExpressionKind(); kind != ExprArithmetic {
+		t.Errorf("Expected ArithmeticExpression %v, got %v", ExprArithmetic, kind)
+	}
+
+	if dataType := arithmeticExpr.ExprType(testEnvironment); dataType != Integer {
+		t.Errorf("ExprType() returned %v, expected %v", dataType, Integer)
+	}
+
+	if anyValue := arithmeticExpr.AsAny(); anyValue != arithmeticExpr {
+		t.Errorf("AsAny() returned %v, expected %v", anyValue, arithmeticExpr)
+	}
+}
+
+func TestComparisonExpressionKind(t *testing.T) {
+	testEnvironment := &Environment{
+		Scopes: make(map[string]DataType),
+	}
+
+	comparisonExpr := &ComparisonExpression{
+		Left: &InExpression{
+			ValuesType: Integer,
+		},
+		Operator: CONullSafeEqual,
+		Right: &InExpression{
+			ValuesType: Integer,
+		},
+	}
+
+	if kind := comparisonExpr.ExpressionKind(); kind != ExprComparison {
+		t.Errorf("Expected ComparisonExpression %v, got %v", ExprComparison, kind)
+	}
+
+	if dataType := comparisonExpr.ExprType(testEnvironment); dataType != Integer {
+		t.Errorf("ExprType() returned %v, expected %v", dataType, Integer)
+	}
+
+	if anyValue := comparisonExpr.AsAny(); anyValue != comparisonExpr {
+		t.Errorf("AsAny() returned %v, expected %v", anyValue, comparisonExpr)
+	}
+}
+
+func TestLikeExpressionKind(t *testing.T) {
+	testEnvironment := &Environment{
+		Scopes: make(map[string]DataType),
+	}
+
+	likeExpr := &LikeExpression{
+		Input: &InExpression{
+			ValuesType: Integer,
+		},
+		Pattern: &InExpression{
+			ValuesType: Integer,
+		},
+	}
+
+	if kind := likeExpr.ExpressionKind(); kind != ExprLike {
+		t.Errorf("Expected LikeExpression %v, got %v", ExprLike, kind)
+	}
+
+	if dataType := likeExpr.ExprType(testEnvironment); dataType != Boolean {
+		t.Errorf("ExprType() returned %v, expected %v", dataType, Boolean)
+	}
+
+	if anyValue := likeExpr.AsAny(); anyValue != likeExpr {
+		t.Errorf("AsAny() returned %v, expected %v", anyValue, likeExpr)
+	}
+}
+
+func TestGlobExpressionKind(t *testing.T) {
+	testEnvironment := &Environment{
+		Scopes: make(map[string]DataType),
+	}
+
+	globExpr := &GlobExpression{
+		Input: &InExpression{
+			ValuesType: Integer,
+		},
+		Pattern: &InExpression{
+			ValuesType: Integer,
+		},
+	}
+
+	if kind := globExpr.ExpressionKind(); kind != ExprGlob {
+		t.Errorf("Expected GlobExpression %v, got %v", ExprGlob, kind)
+	}
+
+	if dataType := globExpr.ExprType(testEnvironment); dataType != Boolean {
+		t.Errorf("ExprType() returned %v, expected %v", dataType, Boolean)
+	}
+
+	if anyValue := globExpr.AsAny(); anyValue != globExpr {
+		t.Errorf("AsAny() returned %v, expected %v", anyValue, globExpr)
+	}
+}
+
+func TestLogicalExpressionKind(t *testing.T) {
+	testEnvironment := &Environment{
+		Scopes: make(map[string]DataType),
+	}
+
+	logicalExpr := &LogicalExpression{
+		Left: &InExpression{
+			ValuesType: Integer,
+		},
+		Operator: Or,
+		Right: &InExpression{
+			ValuesType: Integer,
+		},
+	}
+
+	if kind := logicalExpr.ExpressionKind(); kind != ExprLogical {
+		t.Errorf("Expected LogicalExpression %v, got %v", ExprLogical, kind)
+	}
+
+	if dataType := logicalExpr.ExprType(testEnvironment); dataType != Boolean {
+		t.Errorf("ExprType() returned %v, expected %v", dataType, Boolean)
+	}
+
+	if anyValue := logicalExpr.AsAny(); anyValue != logicalExpr {
+		t.Errorf("AsAny() returned %v, expected %v", anyValue, logicalExpr)
+	}
+}
+
+func TestBitwiseExpressionKind(t *testing.T) {
+	testEnvironment := &Environment{
+		Scopes: make(map[string]DataType),
+	}
+
+	bitwiseExpr := &BitwiseExpression{
+		Left: &InExpression{
+			ValuesType: Integer,
+		},
+		Operator: BOOr,
+		Right: &InExpression{
+			ValuesType: Integer,
+		},
+	}
+
+	if kind := bitwiseExpr.ExpressionKind(); kind != ExprBitwise {
+		t.Errorf("Expected BitwiseExpression %v, got %v", ExprBitwise, kind)
+	}
+
+	if dataType := bitwiseExpr.ExprType(testEnvironment); dataType != Integer {
+		t.Errorf("ExprType() returned %v, expected %v", dataType, Integer)
+	}
+
+	if anyValue := bitwiseExpr.AsAny(); anyValue != bitwiseExpr {
+		t.Errorf("AsAny() returned %v, expected %v", anyValue, bitwiseExpr)
+	}
+}
+
+func TestBetweenExpressionKind(t *testing.T) {
+	testEnvironment := &Environment{
+		Scopes: make(map[string]DataType),
+	}
+
+	betweenExpr := &BetweenExpression{
+		Value: &InExpression{
+			ValuesType: Integer,
+		},
+		RangeStart: &InExpression{
+			ValuesType: Integer,
+		},
+		RangeEnd: &InExpression{
+			ValuesType: Integer,
+		},
+	}
+
+	if kind := betweenExpr.ExpressionKind(); kind != ExprBetween {
+		t.Errorf("Expected BetweenExpression %v, got %v", ExprBetween, kind)
+	}
+
+	if dataType := betweenExpr.ExprType(testEnvironment); dataType != Boolean {
+		t.Errorf("ExprType() returned %v, expected %v", dataType, Boolean)
+	}
+
+	if anyValue := betweenExpr.AsAny(); anyValue != betweenExpr {
+		t.Errorf("AsAny() returned %v, expected %v", anyValue, betweenExpr)
+	}
+}
+
+func TestCaseExpressionKind(t *testing.T) {
+	testEnvironment := &Environment{
+		Scopes: make(map[string]DataType),
+	}
+
+	caseExpr := &CaseExpression{
+		Conditions:   []Expression{&InExpression{ValuesType: Integer}},
+		Values:       []Expression{&InExpression{ValuesType: Integer}},
+		DefaultValue: &InExpression{ValuesType: Integer},
+		ValuesType:   Boolean,
+	}
+
+	if kind := caseExpr.ExpressionKind(); kind != ExprCase {
+		t.Errorf("Expected CaseExpression %v, got %v", ExprCase, kind)
+	}
+
+	if dataType := caseExpr.ExprType(testEnvironment); dataType != Boolean {
+		t.Errorf("ExprType() returned %v, expected %v", dataType, Boolean)
+	}
+
+	if anyValue := caseExpr.AsAny(); anyValue != caseExpr {
+		t.Errorf("AsAny() returned %v, expected %v", anyValue, caseExpr)
+	}
+}
+
+func TestInExpressionKind(t *testing.T) {
+	testEnvironment := &Environment{
+		Scopes: make(map[string]DataType),
+	}
+
+	inExpr := &InExpression{
+		Argument:   &InExpression{ValuesType: Integer},
+		Values:     []Expression{&InExpression{ValuesType: Integer}},
+		ValuesType: Boolean,
+	}
+
+	if kind := inExpr.ExpressionKind(); kind != ExprIn {
+		t.Errorf("Expected InExpression %v, got %v", ExprIn, kind)
+	}
+
+	if dataType := inExpr.ExprType(testEnvironment); dataType != Boolean {
+		t.Errorf("ExprType() returned %v, expected %v", dataType, Boolean)
+	}
+
+	if anyValue := inExpr.AsAny(); anyValue != inExpr {
+		t.Errorf("AsAny() returned %v, expected %v", anyValue, inExpr)
+	}
+}
+
+func TestIsNullExpressionKind(t *testing.T) {
+	testEnvironment := &Environment{
+		Scopes: make(map[string]DataType),
+	}
+
+	isnullExpr := &IsNullExpression{
+		Argument: &InExpression{ValuesType: Integer},
+		HasNot:   true,
+	}
+
+	if kind := isnullExpr.ExpressionKind(); kind != ExprIsNull {
+		t.Errorf("Expected IsNullExpression %v, got %v", ExprIsNull, kind)
+	}
+
+	if dataType := isnullExpr.ExprType(testEnvironment); dataType != Boolean {
+		t.Errorf("ExprType() returned %v, expected %v", dataType, Boolean)
+	}
+
+	if anyValue := isnullExpr.AsAny(); anyValue != isnullExpr {
+		t.Errorf("AsAny() returned %v, expected %v", anyValue, isnullExpr)
+	}
+}
+
+func TestNullExpressionKind(t *testing.T) {
+	testEnvironment := &Environment{
+		Scopes: make(map[string]DataType),
+	}
+
+	nullExpr := &NullExpression{}
+
+	if kind := nullExpr.ExpressionKind(); kind != ExprNull {
+		t.Errorf("Expected NullExpression %v, got %v", ExprNull, kind)
+	}
+
+	if dataType := nullExpr.ExprType(testEnvironment); dataType != Null {
+		t.Errorf("ExprType() returned %v, expected %v", dataType, Null)
+	}
+
+	if anyValue := nullExpr.AsAny(); anyValue != nullExpr {
+		t.Errorf("AsAny() returned %v, expected %v", anyValue, nullExpr)
+	}
+}
