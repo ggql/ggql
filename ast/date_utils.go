a3f9027c625eece4b9594cd4d9511c89e33dad39
Jia Jia <angersax@sina.com>
2024-01-11 23:52:50 +0800

    feat(ast): add support for date_utils

--- ast/date_utils.go
+++ ast/date_utils.go
@@ -1,1 +1,141 @@
 package ast
+
+import (
+	"strconv"
+	"strings"
+	"time"
+)
+
+const (
+	CHRONO_TIME_FORMAT           = "15:04:05"
+	CHRONO_DATE_FORMAT           = "2006-01-02"
+	CHRONO_DATE_TIME_FORMAT      = "2006-01-02 15:04:05"
+	CHRONO_DATE_TIME_FULL_FORMAT = "2006-01-02 15:04:05.000"
+)
+
+// nolint:gomnd
+func GetUnixTimestampMs() int64 {
+	return time.Now().UnixNano() / 1e6
+}
+
+func TimeStampToDate(timeStamp int64) string {
+	t := time.Unix(timeStamp, 0)
+
+	return t.Format(CHRONO_DATE_FORMAT)
+}
+
+func TimeStampToTime(timeStamp int64) string {
+	t := time.Unix(timeStamp, 0)
+
+	return t.Format(CHRONO_TIME_FORMAT)
+}
+
+func TimeStampToDateTime(timeStamp int64) string {
+	t := time.Unix(timeStamp, 0)
+
+	return t.Format(CHRONO_DATE_TIME_FORMAT)
+}
+
+func TimeStampToFullDateTime(timeStamp int64) string {
+	t := time.Unix(timeStamp, 0)
+
+	return t.Format(CHRONO_DATE_TIME_FULL_FORMAT)
+}
+
+func DateToTimeStamp(date string) int64 {
+	t, _ := time.Parse(CHRONO_DATE_FORMAT, date)
+
+	return t.Unix()
+}
+
+func DateTimeToTimeStamp(date string) int64 {
+	data_format := CHRONO_DATE_TIME_FORMAT
+
+	if strings.Contains(date, ".") {
+		data_format = CHRONO_DATE_TIME_FULL_FORMAT
+	}
+
+	t, _ := time.Parse(data_format, date)
+
+	return t.Unix()
+}
+
+func TimeStampFromYearAndDay(year int, dayOfYear uint) int64 {
+	t := time.Date(year, 1, 0, 0, 0, 0, 0, time.UTC)
+	t = t.AddDate(0, 0, int(dayOfYear))
+
+	return t.Unix()
+}
+
+// nolint:gomnd
+func IsValidTimeFormat(timeStr string) bool {
+	var milliseconds int
+
+	// Check length of the string
+	if len(timeStr) < 8 || len(timeStr) > 12 {
+		return false
+	}
+
+	// Split the string into hours, minutes, seconds, and optional milliseconds
+	parts := strings.Split(timeStr, ":")
+	if len(parts) < 3 || len(parts) > 4 {
+		return false
+	}
+
+	// Extract hours, minutes, seconds, and optionally milliseconds
+	hours, _ := strconv.Atoi(parts[0])
+	minutes, _ := strconv.Atoi(parts[1])
+	secondsParts := strings.Split(parts[2], ".")
+	seconds, _ := strconv.Atoi(secondsParts[0])
+
+	if len(secondsParts) == 2 {
+		milliseconds, _ = strconv.Atoi(secondsParts[1])
+	} else {
+		milliseconds = 0
+	}
+
+	// Validate the parsed values
+	return hours >= 0 && hours < 24 &&
+		minutes >= 0 && minutes < 60 &&
+		seconds >= 0 && seconds < 60 &&
+		milliseconds >= 0 && milliseconds < 1000
+}
+
+// nolint:gomnd
+func IsValidDateFormat(dateStr string) bool {
+	// Check length of the string
+	if len(dateStr) != 10 {
+		return false
+	}
+
+	// Split the string into year, month, and day
+	parts := strings.Split(dateStr, "-")
+	if len(parts) != 3 {
+		return false
+	}
+
+	// Extract year, month, and day
+	year, _ := strconv.Atoi(parts[0])
+	month, _ := strconv.Atoi(parts[1])
+	day, _ := strconv.Atoi(parts[2])
+
+	// Validate the parsed values
+	return year >= 1 && month >= 1 && month <= 12 && day >= 1 && day <= 31
+}
+
+// nolint:gomnd
+func IsValidDateTimeFormat(datetimeStr string) bool {
+	// Check length of the string
+	if len(datetimeStr) < 19 || len(datetimeStr) > 23 {
+		return false
+	}
+
+	// Split the string into date and time components
+	parts := strings.Fields(datetimeStr)
+	if len(parts) != 2 {
+		return false
+	}
+
+	// Check the validity of date and time components
+	return IsValidDateFormat(parts[0]) && IsValidTimeFormat(parts[1])
+}
--- ast/date_utils_test.go
+++ ast/date_utils_test.go
@@ -1,1 +1,279 @@
 package ast
+
+import (
+	"testing"
+)
+
+const (
+	TEST_TIME           = "08:30:00"
+	TEST_DATE           = "2024-01-02"
+	TEST_DATE_TIME      = "2024-01-02 08:30:00"
+	TEST_DATE_TIME_FULL = "2024-01-02 08:30:00.000"
+)
+
+func Test_TimeStampFromYearAndDay(t *testing.T) {
+	type args struct {
+		year      int
+		dayOfYear uint
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		// TODO: Add test cases.
+		{name: "", args: args{year: 2024, dayOfYear: 2}, want: 1704153600},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := TimeStampFromYearAndDay(tt.args.year, tt.args.dayOfYear); got != tt.want {
+				t.Errorf("timeStampFromYearAndDay() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTimeStampToDate(t *testing.T) {
+	type args struct {
+		timeStamp int64
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{name: "test1", args: args{timeStamp: 1704153600}, want: TEST_DATE},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := TimeStampToDate(tt.args.timeStamp); got != tt.want {
+				t.Errorf("TimeStampToDate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTimeStampToDateTime(t *testing.T) {
+	type args struct {
+		timeStamp int64
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{name: "test1", args: args{timeStamp: 1704155400}, want: TEST_DATE_TIME},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := TimeStampToDateTime(tt.args.timeStamp); got != tt.want {
+				t.Errorf("TimeStampToDateTime() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTimeStampToFullDateTime(t *testing.T) {
+	type args struct {
+		timeStamp int64
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{name: "test1", args: args{timeStamp: 1704155400}, want: TEST_DATE_TIME_FULL},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := TimeStampToFullDateTime(tt.args.timeStamp); got != tt.want {
+				t.Errorf("TimeStampToFullDateTime() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDateToTimeStamp(t *testing.T) {
+	type args struct {
+		date string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		// TODO: Add test cases.
+		{name: "test1", args: args{date: TEST_DATE}, want: 1704153600},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := DateToTimeStamp(tt.args.date); got != tt.want {
+				t.Errorf("DateToTimeStamp() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDateTimeToTimeStamp(t *testing.T) {
+	type args struct {
+		date string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		// TODO: Add test cases.
+		{name: "test1", args: args{date: TEST_DATE_TIME}, want: 1704184200},
+		{name: "test2", args: args{date: TEST_DATE_TIME_FULL}, want: 1704184200},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := DateTimeToTimeStamp(tt.args.date); got != tt.want {
+				t.Errorf("DateTimeToTimeStamp() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTimeStampToTime(t *testing.T) {
+	type args struct {
+		timeStamp int64
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{name: "test1", args: args{timeStamp: 1704155400}, want: "08:30:00"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := TimeStampToTime(tt.args.timeStamp); got != tt.want {
+				t.Errorf("TimeStampToTime() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTimeStampFromYearAndDay(t *testing.T) {
+	type args struct {
+		year      int
+		dayOfYear uint
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		// TODO: Add test cases.
+		{name: "test1", args: args{year: 2024, dayOfYear: 2}, want: 1704153600},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := TimeStampFromYearAndDay(tt.args.year, tt.args.dayOfYear); got != tt.want {
+				t.Errorf("TimeStampFromYearAndDay() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIsValidTimeFormat(t *testing.T) {
+	type args struct {
+		timeStr string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{name: "test-ok", args: args{timeStr: TEST_TIME}, want: true},
+		{name: "test-err1", args: args{timeStr: "8:30"}, want: false},
+		{name: "test-err2", args: args{timeStr: "8:70"}, want: false},
+		{name: "test-err3", args: args{timeStr: "33:20:00"}, want: false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsValidTimeFormat(tt.args.timeStr); got != tt.want {
+				t.Errorf("IsValidTimeFormat() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIsValidDateFormat(t *testing.T) {
+	type args struct {
+		dateStr string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{name: "test-ok", args: args{dateStr: TEST_DATE}, want: true},
+		{name: "test-err1", args: args{dateStr: "2024-33-04"}, want: false},
+		{name: "test-err2", args: args{dateStr: "2024-01-60"}, want: false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsValidDateFormat(tt.args.dateStr); got != tt.want {
+				t.Errorf("IsValidDateFormat() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIsValidDateTimeFormat(t *testing.T) {
+	type args struct {
+		datetimeStr string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{name: "test-ok1", args: args{datetimeStr: TEST_DATE_TIME}, want: true},
+		{name: "test-ok2", args: args{datetimeStr: TEST_DATE_TIME_FULL}, want: true},
+		{name: "test-err1", args: args{datetimeStr: "2024-33-02 08:30:00"}, want: false},
+		{name: "test-err2", args: args{datetimeStr: "2024-01-53 08:30:00"}, want: false},
+		{name: "test-err3", args: args{datetimeStr: "2024-01-02 35:30:00"}, want: false},
+		{name: "test-err4", args: args{datetimeStr: "2024-01-02 08:87:00"}, want: false},
+		{name: "test-err5", args: args{datetimeStr: "2024-01-02 08:30:99"}, want: false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsValidDateTimeFormat(tt.args.datetimeStr); got != tt.want {
+				t.Errorf("IsValidDateTimeFormat() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
