19f7c8a52e2b00db18cd246eb373042b8d0237a9
Jia <49358172+craftslab@users.noreply.github.com>
2024-04-26 16:30:37 +0800

    feat(engine): upgraded to GQL v0.13.0

--- engine/engine_executor.go
+++ engine/engine_executor.go
@@ -1,1 +1,385 @@
 package engine
+
+import (
+	"errors"
+	"sort"
+
+	"github.com/ggql/ggql/ast"
+)
+
+func ExecuteStatement(
+	env *ast.Environment,
+	statement ast.Statement,
+	repo *Repository,
+	gitqlObject *ast.GitQLObject,
+	aliasTable map[string]string,
+	hiddenSelection []string,
+) error {
+	switch statement.Kind() {
+	case ast.Select:
+		selectStatement := statement.(*ast.SelectStatement)
+		// !!!
+		for _, alias := range selectStatement.AliasTable {
+			aliasTable[alias] = alias
+		}
+		return executeSelectStatement(env, selectStatement, repo, gitqlObject, hiddenSelection)
+	case ast.Where:
+		whereStatement := statement.(*ast.WhereStatement)
+		return executeWhereStatement(env, whereStatement, gitqlObject)
+	case ast.Having:
+		havingStatement := statement.(*ast.HavingStatement)
+		return executeHavingStatement(env, havingStatement, gitqlObject)
+	case ast.Limit:
+		limitStatement := statement.(*ast.LimitStatement)
+		return executeLimitStatement(limitStatement, gitqlObject)
+	case ast.Offset:
+		offsetStatement := statement.(*ast.OffsetStatement)
+		return executeOffsetStatement(offsetStatement, gitqlObject)
+	case ast.OrderBy:
+		orderByStatement := statement.(*ast.OrderByStatement)
+		return executeOrderByStatement(env, orderByStatement, gitqlObject)
+	case ast.GroupBy:
+		groupByStatement := statement.(*ast.GroupByStatement)
+		return executeGroupByStatement(groupByStatement, gitqlObject)
+	case ast.AggregateFunction:
+		aggregationsStatement := statement.(*ast.AggregationsStatement)
+		return executeAggregationFunctionStatement(env, aggregationsStatement, gitqlObject, aliasTable)
+	case ast.GlobalVariable:
+		globalVariableStatement := statement.(*ast.GlobalVariableStatement)
+		return executeGlobalVariableStatement(env, globalVariableStatement)
+	default:
+		return errors.New("unknown statement kind")
+	}
+}
+
+func executeSelectStatement(
+	env *ast.Environment,
+	statement *ast.SelectStatement,
+	repo *Repository,
+	gitqlObject *ast.GitQLObject,
+	hiddenSelections []string,
+) error {
+	fieldsNames := statement.FieldsNames
+	if statement.TableName != "" {
+		for _, hidden := range hiddenSelections {
+			if !contains(fieldsNames, hidden) {
+				fieldsNames = append(fieldsNames, hidden)
+			}
+		}
+	}
+
+	for _, fieldName := range fieldsNames {
+		gitqlObject.Titles = append(gitqlObject.Titles, getColumnName(statement.AliasTable, fieldName))
+	}
+
+	objects, err := selectGQLObjects(env, repo, statement.TableName, fieldsNames, gitqlObject.Titles, statement.FieldsValues)
+	if err != nil {
+		return err
+	}
+
+	if gitqlObject.IsEmpty() {
+		gitqlObject.Groups = append(gitqlObject.Groups, objects)
+	} else {
+		gitqlObject.Groups[0].Rows = append(gitqlObject.Groups[0].Rows, objects.Rows...)
+	}
+
+	return nil
+}
+
+func executeWhereStatement(
+	env *ast.Environment,
+	statement *ast.WhereStatement,
+	gitqlObject *ast.GitQLObject,
+) error {
+	if gitqlObject.IsEmpty() {
+		return nil
+	}
+
+	filteredGroup := ast.Group{}
+	firstGroup := gitqlObject.Groups[0].Rows
+	for _, object := range firstGroup {
+		// !!!
+		evalResult, err := evaluateExpression(env, statement.Condition, gitqlObject.Titles, object.Values)
+		if err != nil {
+			return err
+		}
+
+		if evalResult.AsBool() {
+			filteredGroup.Rows = append(filteredGroup.Rows, ast.Row{Values: object.Values})
+		}
+	}
+
+	gitqlObject.Groups = gitqlObject.Groups[1:]
+	gitqlObject.Groups = append(gitqlObject.Groups, filteredGroup)
+
+	return nil
+}
+
+func executeHavingStatement(
+	env *ast.Environment,
+	statement *ast.HavingStatement,
+	gitqlObject *ast.GitQLObject,
+) error {
+	if gitqlObject.IsEmpty() {
+		return nil
+	}
+
+	if len(gitqlObject.Groups) > 1 {
+		gitqlObject.Flat()
+	}
+
+	filteredGroup := ast.Group{}
+	firstGroup := gitqlObject.Groups[0].Rows
+	for _, object := range firstGroup {
+		evalResult, err := evaluateExpression(env, statement.Condition, gitqlObject.Titles, object.Values)
+		if err != nil {
+			return err
+		}
+
+		if evalResult.AsBool() {
+			filteredGroup.Rows = append(filteredGroup.Rows, ast.Row{Values: object.Values})
+		}
+	}
+
+	gitqlObject.Groups = gitqlObject.Groups[1:]
+	gitqlObject.Groups = append(gitqlObject.Groups, filteredGroup)
+
+	return nil
+}
+
+func executeLimitStatement(
+	statement *ast.LimitStatement,
+	gitqlObject *ast.GitQLObject,
+) error {
+	if gitqlObject.IsEmpty() {
+		return nil
+	}
+
+	if len(gitqlObject.Groups) > 1 {
+		gitqlObject.Flat()
+	}
+
+	mainGroup := &gitqlObject.Groups[0]
+	if statement.Count <= len(mainGroup.Rows) {
+		mainGroup.Rows = mainGroup.Rows[:statement.Count]
+	}
+
+	return nil
+}
+
+func executeOffsetStatement(
+	statement *ast.OffsetStatement,
+	gitqlObject *ast.GitQLObject,
+) error {
+	if gitqlObject.IsEmpty() {
+		return nil
+	}
+
+	if len(gitqlObject.Groups) > 1 {
+		gitqlObject.Flat()
+	}
+
+	mainGroup := &gitqlObject.Groups[0]
+	if statement.Count <= len(mainGroup.Rows) {
+		mainGroup.Rows = mainGroup.Rows[statement.Count:]
+	} else {
+		mainGroup.Rows = nil
+	}
+
+	return nil
+}
+
+func executeOrderByStatement(
+	env *ast.Environment,
+	statement *ast.OrderByStatement,
+	gitqlObject *ast.GitQLObject,
+) error {
+	if gitqlObject.IsEmpty() {
+		return nil
+	}
+
+	if len(gitqlObject.Groups) > 1 {
+		gitqlObject.Flat()
+	}
+
+	mainGroup := &gitqlObject.Groups[0]
+	if len(mainGroup.Rows) == 0 {
+		return nil
+	}
+
+	sort.SliceStable(mainGroup.Rows, func(i, j int) bool {
+		for idx := range statement.Arguments {
+			argument := statement.Arguments[idx]
+			if argument.IsConst() {
+				continue
+			}
+
+			first, err := evaluateExpression(env, argument, gitqlObject.Titles, mainGroup.Rows[i].Values)
+			if err != nil {
+				return false
+			}
+
+			other, err := evaluateExpression(env, argument, gitqlObject.Titles, mainGroup.Rows[j].Values)
+			if err != nil {
+				return false
+			}
+
+			currentOrdering := compare(first, other)
+
+			if currentOrdering == 0 {
+				continue
+			}
+
+			if statement.SortingOrders[idx] == ast.Descending {
+				return currentOrdering > 0
+			} else {
+				return currentOrdering < 0
+			}
+		}
+
+		return false
+	})
+
+	return nil
+}
+
+func executeGroupByStatement(
+	statement *ast.GroupByStatement,
+	gitqlObject *ast.GitQLObject,
+) error {
+	if gitqlObject.IsEmpty() {
+		return nil
+	}
+
+	mainGroup := gitqlObject.Groups[0]
+	if len(mainGroup.Rows) == 0 {
+		return nil
+	}
+
+	groupsMap := make(map[string]int)
+	nextGroupIndex := 0
+
+	for _, object := range mainGroup.Rows {
+		fieldIndex := indexOf(gitqlObject.Titles, statement.FieldName)
+		fieldValue := object.Values[fieldIndex].Fmt()
+		if _, ok := groupsMap[fieldValue]; !ok {
+			groupsMap[fieldValue] = nextGroupIndex
+			nextGroupIndex++
+			gitqlObject.Groups = append(gitqlObject.Groups, ast.Group{Rows: []ast.Row{object}})
+		} else {
+			index := groupsMap[fieldValue]
+			targetGroup := &gitqlObject.Groups[index]
+			targetGroup.Rows = append(targetGroup.Rows, object)
+		}
+	}
+
+	return nil
+}
+
+// !!!
+func executeAggregationFunctionStatement(
+	env *ast.Environment,
+	statement *ast.AggregationsStatement,
+	gitqlObject *ast.GitQLObject,
+	aliasTable map[string]string,
+) error {
+	if len(statement.Aggregations) == 0 {
+		return nil
+	}
+
+	return nil
+}
+
+func executeGlobalVariableStatement(
+	env *ast.Environment,
+	statement *ast.GlobalVariableStatement,
+) error {
+	value, err := evaluateExpression(env, nil, nil, nil)
+	if err != nil {
+		return err
+	}
+
+	env.Globals[statement.Name] = value
+	return nil
+}
+
+// !!!
+func selectGQLObjects(
+	env *ast.Environment,
+	repo *Repository,
+	tableName string,
+	fieldsNames []string,
+	titles []string,
+	fieldsValues []ast.Expression,
+) (ast.Group, error) {
+	objects := ast.Group{}
+	return objects, nil
+}
+
+// !!!
+func evaluateExpression(
+	env *ast.Environment,
+	expression ast.Expression,
+	titles []string,
+	values []ast.Value,
+) (ast.Value, error) {
+	var a ast.Value
+	return a, nil
+}
+
+func contains(slice []string, item string) bool {
+	for _, s := range slice {
+		if s == item {
+			return true
+		}
+	}
+	return false
+}
+
+func getColumnName(aliasTable map[string]string, columnName string) string {
+	if alias, ok := aliasTable[columnName]; ok {
+		return alias
+	}
+	return columnName
+}
+
+func indexOf(slice []string, item string) int {
+	for i, s := range slice {
+		if s == item {
+			return i
+		}
+	}
+	return -1
+}
+
+func compare(a, b interface{}) int {
+	switch a := a.(type) {
+	case int:
+		b := b.(int)
+		if a < b {
+			return -1
+		} else if a > b {
+			return 1
+		} else {
+			return 0
+		}
+	case float64:
+		b := b.(float64)
+		if a < b {
+			return -1
+		} else if a > b {
+			return 1
+		} else {
+			return 0
+		}
+	case string:
+		b := b.(string)
+		if sort.StringsAreSorted([]string{a, b}) {
+			return 1
+		} else {
+			return 0
+		}
+	default:
+		return 0
+	}
+}
