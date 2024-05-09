1273e7d9b0ddd626a3077905e292d66e4a540e27
Jia Jia <angersax@sina.com>
2024-05-10 05:52:35 +0800

    feat(engine): add test cases

--- engine/engine_function_test.go
+++ engine/engine_function_test.go
@@ -1,8 +1,20 @@
 package engine
 
 import (
+	"os"
 	"testing"
+
+	"github.com/go-git/go-git/v5"
 )
+
+func newFunctionRepo(path string) error {
+	_, err := git.PlainInit(path, true)
+	return err
+}
+
+func deleteFunctionRepo(path string) error {
+	return os.RemoveAll(path)
+}
 
 func TestSelectGQLObjects(t *testing.T) {
 }
