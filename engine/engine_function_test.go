92dc0a3ec4d8e71e9ca58b7dfd327fafbcac8b48
Jia <49358172+craftslab@users.noreply.github.com>
2024-05-11 09:27:55 +0000

    feat(engine): add test cases

--- engine/engine_function_test.go
+++ engine/engine_function_test.go
@@ -2,38 +2,53 @@ package engine
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/config"
-	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/ggql/ggql/ast"
 )
 
 const (
+	functionFile = "ggql-engine-function-test.txt"
 	functionRepo = "ggql-engine-function-test.git"
 
-	tableCommits = "commits"
+	tableName = "refs"
 )
 
 func newFunctionRepo() *git.Repository {
-	_, _ = git.PlainInit(functionRepo, true)
+	// Create a new repository
+	_, _ = git.PlainInit(functionRepo, false)
 	repo, _ := git.PlainOpen(functionRepo)
 
-	branch := config.Branch{
-		Name: "master",
-	}
-	_ = repo.CreateBranch(&branch)
+	tree, _ := repo.Worktree()
 
-	name := "v0.0.1"
-	hash := plumbing.Hash{}
-	_, _ = repo.CreateTag(name, hash, nil)
+	// Create a new file
+	filePath := filepath.Join(tree.Filesystem.Root(), functionFile)
+	file, _ := os.Create(filePath)
+	_, _ = file.WriteString("hello world")
+	_ = file.Close()
 
-	_, _ = repo.CreateRemote(&config.RemoteConfig{
-		Name: "function-remote",
-		URLs: []string{"https://github.com/function-project/" + functionRepo},
+	// Create a new commit
+	_, _ = tree.Add(functionFile)
+	commit, _ := tree.Commit("Adding "+functionFile, &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  "name",
+			Email: "name@example.com",
+			When:  time.Now(),
+		},
+	})
+
+	_, _ = repo.CommitObject(commit)
+
+	// Create a new tag
+	ref, _ := repo.Head()
+	_, _ = repo.CreateTag("v0.0.1", ref.Hash(), &git.CreateTagOptions{
+		Message: "Create tag",
 	})
 
 	return repo
@@ -53,11 +68,16 @@ func TestSelectGQLObjects(t *testing.T) {
 	repo := newFunctionRepo()
 	defer deleteFunctionRepo()
 
-	table := tableCommits
-	fieldsNames := []string{"commit_id", "name", "email", "title", "message", "datetime", "repo"}
+	table := tableName
+	fieldsNames := []string{"name", "full_name", "type", "repo"}
 
-	var titles []string
-	var fieldsValues []ast.Expression
+	titles := []string{"title"}
+	fieldsValues := []ast.Expression{
+		&ast.StringExpression{
+			Value:     "value",
+			ValueType: ast.StringValueText,
+		},
+	}
 
 	_, err := SelectGQLObjects(&env, repo, table, fieldsNames, titles, fieldsValues)
 	assert.Equal(t, nil, err)
@@ -73,86 +93,145 @@ func TestSelectReferences(t *testing.T) {
 	repo := newFunctionRepo()
 	defer deleteFunctionRepo()
 
-	fieldsNames := []string{"name"}
-	titles := []string{"value"}
+	fieldsNames := []string{"name", "full_name", "type", "repo"}
+	titles := []string{"title"}
 	fieldsValues := []ast.Expression{
-		&ast.StringExpression{
-			Value:     "value",
-			ValueType: ast.StringValueText,
+		&ast.SymbolExpression{
+			Value: "value",
 		},
 	}
 
 	group, err := selectReferences(&env, repo, fieldsNames, titles, fieldsValues)
 	assert.Equal(t, nil, err)
-	assert.Equal(t, 2, len(group.Rows))
-	assert.Equal(t, 1, len(group.Rows[0].Values))
-	assert.Equal(t, "value", group.Rows[0].Values[0].Fmt())
-	assert.Equal(t, 1, len(group.Rows[1].Values))
-	assert.Equal(t, "value", group.Rows[1].Values[0].Fmt())
-
-	fieldsNames = []string{"name"}
-	titles = []string{"value"}
-	fieldsValues = []ast.Expression{
-		&ast.SymbolExpression{
-			Value: "value",
-		},
-	}
-
-	group, err = selectReferences(&env, repo, fieldsNames, titles, fieldsValues)
-	assert.Equal(t, nil, err)
-	assert.Equal(t, 2, len(group.Rows))
-	assert.Equal(t, 1, len(group.Rows[0].Values))
-	assert.Equal(t, "v0.0.1", group.Rows[0].Values[0].Fmt())
-	assert.Equal(t, 1, len(group.Rows[1].Values))
-	assert.Equal(t, "HEAD", group.Rows[1].Values[0].Fmt())
-
-	fieldsNames = []string{"full_name"}
-	titles = []string{"value"}
-	fieldsValues = []ast.Expression{
-		&ast.SymbolExpression{
-			Value: "value",
-		},
-	}
-
-	group, err = selectReferences(&env, repo, fieldsNames, titles, fieldsValues)
-	assert.Equal(t, nil, err)
-	assert.Equal(t, 2, len(group.Rows))
-	assert.Equal(t, 1, len(group.Rows[0].Values))
-	assert.Equal(t, "refs/tags/v0.0.1", group.Rows[0].Values[0].Fmt())
-	assert.Equal(t, 1, len(group.Rows[1].Values))
-	assert.Equal(t, "HEAD", group.Rows[1].Values[0].Fmt())
-
-	fieldsNames = []string{"type"}
-	titles = []string{"value"}
-	fieldsValues = []ast.Expression{
-		&ast.SymbolExpression{
-			Value: "value",
-		},
-	}
-
-	group, err = selectReferences(&env, repo, fieldsNames, titles, fieldsValues)
-	assert.Equal(t, nil, err)
-	assert.Equal(t, 2, len(group.Rows))
-	assert.Equal(t, 1, len(group.Rows[0].Values))
-	assert.Equal(t, "tag", group.Rows[0].Values[0].Fmt())
-	assert.Equal(t, 1, len(group.Rows[1].Values))
-	assert.Equal(t, "other", group.Rows[1].Values[0].Fmt())
+	assert.Equal(t, 3, len(group.Rows))
+	assert.Equal(t, len(fieldsNames), len(group.Rows[0].Values))
 }
 
 func TestSelectCommits(t *testing.T) {
+	env := ast.Environment{
+		Globals:      map[string]ast.Value{},
+		GlobalsTypes: map[string]ast.DataType{},
+		Scopes:       map[string]ast.DataType{},
+	}
+
+	repo := newFunctionRepo()
+	defer deleteFunctionRepo()
+
+	fieldsNames := []string{"commit_id", "name", "email", "title", "message", "datetime", "repo"}
+	titles := []string{"title"}
+	fieldsValues := []ast.Expression{
+		&ast.SymbolExpression{
+			Value: "value",
+		},
+	}
+
+	group, err := selectCommits(&env, repo, fieldsNames, titles, fieldsValues)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 1, len(group.Rows))
+	assert.Equal(t, len(fieldsNames), len(group.Rows[0].Values))
 }
 
 func TestSelectBranches(t *testing.T) {
+	env := ast.Environment{
+		Globals:      map[string]ast.Value{},
+		GlobalsTypes: map[string]ast.DataType{},
+		Scopes:       map[string]ast.DataType{},
+	}
+
+	repo := newFunctionRepo()
+	defer deleteFunctionRepo()
+
+	fieldsNames := []string{"name", "commit_count", "is_head", "is_remote", "repo"}
+	titles := []string{"title"}
+	fieldsValues := []ast.Expression{
+		&ast.SymbolExpression{
+			Value: "value",
+		},
+	}
+
+	group, err := selectBranches(&env, repo, fieldsNames, titles, fieldsValues)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 1, len(group.Rows))
+	assert.Equal(t, len(fieldsNames), len(group.Rows[0].Values))
 }
 
 func TestSelectDiffs(t *testing.T) {
+	env := ast.Environment{
+		Globals:      map[string]ast.Value{},
+		GlobalsTypes: map[string]ast.DataType{},
+		Scopes:       map[string]ast.DataType{},
+	}
+
+	repo := newFunctionRepo()
+	defer deleteFunctionRepo()
+
+	fieldsNames := []string{"commit_id", "name", "email", "repo", "insertions", "deletions", "files_changed"}
+	titles := []string{"title"}
+	fieldsValues := []ast.Expression{
+		&ast.SymbolExpression{
+			Value: "value",
+		},
+	}
+
+	group, err := selectDiffs(&env, repo, fieldsNames, titles, fieldsValues)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 1, len(group.Rows))
+	assert.Equal(t, len(fieldsNames), len(group.Rows[0].Values))
 }
 
 func TestSelectTags(t *testing.T) {
+	env := ast.Environment{
+		Globals:      map[string]ast.Value{},
+		GlobalsTypes: map[string]ast.DataType{},
+		Scopes:       map[string]ast.DataType{},
+	}
+
+	repo := newFunctionRepo()
+	defer deleteFunctionRepo()
+
+	fieldsNames := []string{"name", "repo"}
+	titles := []string{"title"}
+	fieldsValues := []ast.Expression{
+		&ast.SymbolExpression{
+			Value: "value",
+		},
+	}
+
+	group, err := selectTags(&env, repo, fieldsNames, titles, fieldsValues)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 1, len(group.Rows))
+	assert.Equal(t, len(fieldsNames), len(group.Rows[0].Values))
 }
 
 func TestSelectValues(t *testing.T) {
+	env := ast.Environment{
+		Globals:      map[string]ast.Value{},
+		GlobalsTypes: map[string]ast.DataType{},
+		Scopes:       map[string]ast.DataType{},
+	}
+
+	titles := []string{"title"}
+	fieldsValues := []ast.Expression{
+		&ast.SymbolExpression{
+			Value: "value",
+		},
+	}
+
+	group, err := selectValues(&env, titles, fieldsValues)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 1, len(group.Rows))
 }
 
 func TestGetColumnName(t *testing.T) {
+	aliasTable := map[string]string{
+		"key": "value",
+	}
+
+	name := "key"
+	ret := GetColumnName(aliasTable, name)
+	assert.Equal(t, aliasTable[name], ret)
+
+	name = "invalid"
+	ret = GetColumnName(aliasTable, name)
+	assert.Equal(t, name, ret)
 }
